a72590f0fba6926869f371cce2e1800ed661f16f
S <s>
2015-04-23 14:37:04 -0400

    {"type":"TI", "runs":"8"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -10,16 +10,14 @@ import stanford.karel.*;
  */
 public class StoneMasonKarel extends SuperKarel {
 	public void run() {
-		int j=1;
-		while (j!=6) {			/*This counter makes sure Karel would not go to higher streets than intended */
+		while (leftIsCLear()) {			/*This counter makes sure Karel would not go to higher streets than intended */
 		if (frontIsClear()) {   /* This condition makes sure Karel is not at the end of the street*/
 			CheckForTile();     /*Karel checks if there is a tile in place then moves on, if not puts one */
 			MoveToNextPlace(); 	/* This method moves Karel to avenues 4 blocks apart*/
 		} else {
 			CheckForTile();   /*This was put into place for the Off_By_One error*/
 			ComeBack();      /* Makes Karel to come back to first avenue*/
-			GoToNextRow();   /* Makes Karel to go up one street*/
-			j++;
+			GoToNextColumn();   /* Makes Karel to go up one street*/
 		}
 	}
 	}
@@ -36,7 +34,7 @@ private void MoveToNextPlace() {
 	}
 	
 	}
-private void GoToNextRow() {
+private void GoToNextColumn() {
 	turnLeft();
 	if (frontIsClear()) {
 		move();
