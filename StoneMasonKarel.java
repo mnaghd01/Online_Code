24464a94d5c1a7541dec501d68260b5c2bcf31ae
S <s>
2015-04-23 15:21:55 -0400

    {"type":"TI", "runs":"0"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -19,7 +19,7 @@ public class StoneMasonKarel extends SuperKarel {
 	}
 
 	private void RepairTheTiles(){
-		while (leftIsClear()){
+		while (frontIsClear()){
 		MoveUp();   		  		/*Karel checks if there is a tile in place then moves on, if not puts one */
 		MoveDown();     			 /* Makes Karel to come back to first avenue*/
 		MoveToNextPlace(); 			/* This method moves Karel to avenues 4 blocks apart*/
