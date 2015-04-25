bba0cc5b54e39dd000bceea6619995508d31f217
S <s>
2015-04-25 14:44:43 -0400

    {"type":"TI", "runs":"2"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -13,8 +13,7 @@ public class CheckerboardKarel extends SuperKarel {
 	public void run() {
 		while (true) { /*Checks to see if it has reached the top*/
 			if (leftIsClear()){
-			PutaRow();
-			ComeBack();/* Puts a beeper in a row back and forth*/
+			PutaRow();/* Puts a beeper in a row back and forth*/
 			goUp();         /*If reached at the end of a street, goes up one avenue*/
 		} else{
 			if (frontIsClear()) {
