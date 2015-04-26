11b26e66e41c9d634eef5ccb450a428f6b39e073
S <s>
2015-04-26 18:08:11 -0400

    {"type":"TI", "runs":"0"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -16,52 +16,37 @@ import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 	public void run() {
-		int i = 1;
-		int j=1;
-		while (leftIsClear()) { /*Two while loops check the number of the streets (j) and avenues (i) respectively.*/
-			j = 1;
-			while (frontIsClear()) {
-				if ((i ^ 2 + j ^ 2) % 2 != 0) {
-					putBeeper();
-					move();
-					j++;
-				} else {
-					move();
-					j++;
-				}
-			}
-			if ((i ^ 2 + j ^ 2) % 2 != 0){ /* The condition was put in place in case Karel reached the end of the street and did not put beeper were it should*/
-				putBeeper();
-			}
-			j = 1;
-			GoBack();
+		int i=0;
+		int j=0;
+			while(frontIsClear()){
+			move();
 			i++;
 		}
-		while (frontIsClear()) { /* This condition is put in place in case Karel reached the top of the "world" and did not put the beepers due to OBOBug*/
-			if ((i ^ 2 + j ^ 2) % 2 != 0) {
-				putBeeper();
-				move();
-				i++;
-			} else {
-				move();
-				i++;
+			while (leftIsClear()){
+			turnLeft();
+			move();
+			j++;
 			}
-		}
-		if ((i ^ 2 + j ^ 2) % 2 != 0)	{/* This condition is in place just for the very end block at the top right of the world where Karel encounters another OBOBug*/
-			putBeeper();
-		}
-	}
-
-	private void GoBack() { /*This method moves Karel from one street to another one and turns and faces it in the correct west to east direction*/
+			while (j!=0){
+				
+			int k=i;
+			while (i!=0){
+				if ((i^2+j^2)%2!=0){
+					putBeeper();
+					move();
+					i--;
+				}else {
+					move();
+					i--;
+				}
+			}
+			i=k;
+			GoBack();
+			}
+			
+	private void GoBack() {
 		turnLeft();
 		turnLeft();
-		while (frontIsClear()) {
-			move();
-		}
-		turnRight();
-		if (frontIsClear()) {
-			move();
-		}
-		turnRight();
 	}
-}
+	}
+	}
