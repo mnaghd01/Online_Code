80446d637bf40aed513cefa7c4f112069121deb6
S <s>
2015-04-26 19:11:36 -0400

    {"type":"TI", "runs":"1"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -17,7 +17,7 @@ import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
 	public void run() {
-		int i = 1;
+		int i = 0;
 		int j = 1;
 		while (frontIsClear()) {
 			move();
